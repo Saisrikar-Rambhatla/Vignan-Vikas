710a9001887d3503bb00cc3aba6cd3cb3675ebb4
saisr <sskar1105@gmail.com>
2022-10-14 19:51:13 +0530

    Hey i am done

---
+++ app/src/main/java/com/example/vignanvikas2/MainActivity.java
@@ -0,0 +1,18 @@
+  package com.example.vignanvikas2;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+
+import android.os.Bundle;
+import android.view.View;
+
+  public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+      CardView uploadNotice;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        uploadNotice = findViewById(R.id.addNotice);
+
+    }
+}
