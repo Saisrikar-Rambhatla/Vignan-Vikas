6e09dc4bda5418e0a487868f1b5e2dca6d9ff119
saisr <sskar1105@gmail.com>
2022-10-22 12:18:49 +0530

    Hey i am the project

---
+++ app/src/main/java/com/example/vignanvikas2/Upload_Attendence.java
@@ -0,0 +1,14 @@
+package com.example.vignanvikas2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Upload_Attendence extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_upload_attendence);
+    }
+}
